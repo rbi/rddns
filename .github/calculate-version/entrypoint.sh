cfb5193ef3629a5c7dd7f4d14903a38c9ea8c015
Raik Bieniek <raik@voidnode.de>
2019-04-14 10:12:07 +0200

    Do not fail to calculate the build version when target directory does not exist.

--- .github/calculate-version/entrypoint.sh
+++ .github/calculate-version/entrypoint.sh
@@ -2,5 +2,10 @@
 
 BASE_VERSION=`cat Cargo.toml | grep version | sed 's/^[^"]*"\([^"]*\).*$/\1/'`
 
+
+if [ ! -d target ]; then
+  mkdir target
+fi
+
 echo v$BASE_VERSION.`date -u +%s`
 echo v$BASE_VERSION.`date -u +%s` > target/build-version
--- .github/calculate-version/test.sh
+++ .github/calculate-version/test.sh
@@ -3,5 +3,5 @@
 BASE_DIR_RELATIVE=`dirname "$0"`/../..
 BASE_DIR=`realpath ${BASE_DIR_RELATIVE}`
 
-docker build -t rbi/calculate-version .
+docker build -t rbi/calculate-version $BASE_DIR/.github/calculate-version
 docker run -v ${BASE_DIR}:/github/workdir -w /github/workdir --rm rbi/calculate-version
