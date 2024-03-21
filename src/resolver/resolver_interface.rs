13323b937388e0b1e98bcf501ac6aa145e7668a8
MarkusTieger <markustieger@gmail.com>
2024-03-21 16:55:59 +0100

    Added a regex feature to the interface resolver

--- src/resolver/resolver_interface.rs
+++ src/resolver/resolver_interface.rs
@@ -2,6 +2,7 @@ use crate::config::IpAddressInterface;
 use ipnetwork::IpNetwork;
 use pnet::datalink::{interfaces, NetworkInterface};
 use std::net::IpAddr;
+use regex::{Error, Regex};
 
 pub fn resolve_interface(config: &IpAddressInterface) -> Option<IpAddr> {
     config
@@ -15,15 +16,25 @@ pub fn resolve_interface(config: &IpAddressInterface) -> Option<IpAddr> {
         })
         .ok()
         .and_then(|network| {
-            get_interface(&config.interface).and_then(|iface| get_ip_address(&iface, &network))
+            get_interface(&config.interface, config.regex).and_then(|iface| get_ip_address(&iface, &network))
         })
 }
 
-fn get_interface(name: &str) -> Option<NetworkInterface> {
-    interfaces()
-        .into_iter()
-        .filter(|iface| iface.name == name)
-        .next()
+fn get_interface(name: &str, regex: bool) -> Option<NetworkInterface> {
+    if regex {
+        match Regex::new(name) {
+            Ok(regex) => interfaces()
+                .into_iter()
+                .filter(|iface| regex.is_match(&iface.name))
+                .next(),
+            Err(err) => None,
+        }
+    } else {
+        interfaces()
+            .into_iter()
+            .filter(|iface| iface.name == name)
+            .next()
+    }
 }
 
 fn get_ip_address(iface: &NetworkInterface, expected_network: &IpNetwork) -> Option<IpAddr> {
