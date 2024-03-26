bd7bf2afa5a30773c89ad27d7449e1172ba16ce6
Raik Bieniek <raik@bieniek-it.de>
2024-03-26 10:07:03 +0100

    Delete Github workflow definitions from the time it was in beta.
    These are no longer working.

--- .github/calculate-version/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-BASE_DIR_RELATIVE=`dirname "$0"`/../..
-BASE_DIR=`realpath ${BASE_DIR_RELATIVE}`
-
-docker build -t rbi/calculate-version $BASE_DIR/.github/calculate-version
-docker run -v ${BASE_DIR}:/github/workdir -w /github/workdir --rm rbi/calculate-version
